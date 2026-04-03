38a632cb32b0a850f7a0d781aff40193858b2505
Trần Trung Kiên <trungkien2981412@gmail.com>
2026-04-03 16:09:56 +0700

    test cicd

--- src/pages/api/admin/users/index.ts
+++ src/pages/api/admin/users/index.ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       
       // Don't send back passwordHash
       const userRes = newUser.toObject();
-      delete userRes.passwordHash;
+      delete (userRes as any).passwordHash;
 
       return res.status(201).json({ success: true, data: userRes });
     }
